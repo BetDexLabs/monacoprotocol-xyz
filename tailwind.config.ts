f11417230c814810a6633bf700b324595be5ba4c
Yuliia Cheliak <yuliia@betdex.com>
2024-10-17 11:44:13 +0300

    Add style configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,29 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        typography: {
+          primary: "var(--white)",
+          secondary: "var(--blue-500)",
+          tertiary: "var(--gray-400)",
+          "heading-dark": "var(--gradient-blue-to-purple)",
+          "heading-light": "var(--gradient-light-blue-to-purple)",
+          "heading-neutral": "var(--gradient-white-to-gray)",
+        },
+        background: {
+          main: "var(--gradient-dark-purple)",
+          accent: "var(--gradient-blue-to-purple)"
+        },
+      },
+      fontSize: {
+        h1: ['125px', { lineHeight: '125px', fontWeight: '600', letterSpacing: '-0.05em' }],
+        h2: ['48px', { lineHeight: '56px', fontWeight: '600', letterSpacing: '-0.05em' }],
+        h3: ['30px', { lineHeight: '36px', fontWeight: '600', letterSpacing: '0' }],
+        body: ['24px', { lineHeight: '34px', fontWeight: '400', letterSpacing: '-0.02em' }]
+      },
+      screens: {
+        sm: '320px',
+        md: '672px',
+        xl: '1920px'
       },
     },
   },
